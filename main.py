cc3746d9a4650f3789f10567ef654476f6d25dff
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-09-01 22:42:12 +0200

    keep track of the actual message being sent (required to figure out text width and how to position selection marker)

--- main.py
+++ main.py
@@ -2,35 +2,46 @@ import keyboard
 
 in_chat = False
 selection_index = 0
-message_length = 0
+message = ""
+
+def remove_char_at_index(s, index):
+    return s[:index] + s[index+1:]
+
+def insert_char_at_index(s, index, char):
+    return s[:index] + char + s[index:]
 
 def on_key_press(event:keyboard.KeyboardEvent):
-    global in_chat, selection_index, message_length
+    global in_chat, selection_index, message
     if event.name == "escape":
         in_chat = False
-    if event.name == "enter":
+        message = ""
+        selection_index = 0
+    elif event.name == "enter":
         in_chat = not in_chat
+        if not in_chat:
+            message = ""
+            selection_index = 0
     elif in_chat and len(event.name) == 1 or event.name == "space":
+        message = insert_char_at_index(message, selection_index, event.name if event.name != "space" else " ")
         selection_index += 1
-        message_length += 1
     elif event.name == "left":
         if selection_index > 0:
             selection_index -= 1
     elif event.name == "right":
-        if selection_index < message_length:
+        if selection_index < len(message):
             selection_index += 1
     elif event.name == "home":
         selection_index = 0
     elif event.name == "end":
-        selection_index = message_length
+        selection_index = len(message)
     elif event.name == "backspace":
         if selection_index > 0:
-            message_length -= 1
             selection_index -= 1
+            message = remove_char_at_index(message, selection_index)
     elif event.name == "delete":
-        if selection_index < message_length:
-            message_length -= 1
-    print(f"{message_length=}")
+        if selection_index < len(message):
+            message = remove_char_at_index(message, selection_index)
+    print(f"{message=}")
     print(f"{selection_index=}")
 
 keyboard.on_press(on_key_press)
